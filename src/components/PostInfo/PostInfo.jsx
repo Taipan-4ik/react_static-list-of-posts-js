b18e2f7c89b2bacd9cf5355dda103aa24a82ca79
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-04-16 09:14:30 +0200

    add task solutinon

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,30 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { comments } from '../../api/data';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>{' Posted by  '}</p>
+
+      <UserInfo user={post.user} />
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    {comments
+      .filter(comment => comment.postId === post.id)
+      .map(comment => (
+        <CommentList comment={comment} key={comment.id} />
+      ))}
+
+    {comments.every(comment => comment.postId !== post.id) && (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
+);
